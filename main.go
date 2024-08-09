057e10f1bfd3fd35e91258f1091c857b60f6b74e
kamataryo <mugil.cephalus+github@gmail.com>
2024-08-09 09:13:06 +0900

    Add external

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	say, err := cowsay.Say("Hello, World!")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(say)
 }
